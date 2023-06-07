8c90a3370ba381b0f776161e2d78305fa7aaf12b
Austin Glaves <36416730+aglaves@users.noreply.github.com>
2023-06-07 14:14:01 -0500

    Split ejs files into partials.

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.listen(8888);
 
+const blogs = [
+    {title: 'Mr T. vs Bart Simpson', snippet: 'Mr. T crushes Bart!'},
+    {title: 'Mr T. vs Rocky', snippet: 'Rockey gets a lot of pain.'},
+    {title: 'Mr T. vs Yo Mama', snippet: 'No.'}
+]
+
 app.get('/', (request, response) => {
-    response.sendFile('./views/index.html', { root: __dirname });
+    response.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (request, response) => {
-    response.sendFile('./views/about.html', { root: __dirname });
+    response.render('about', {title: 'About'});
 });
 
 app.get('/about-node.js', (request, response) => {
     response.redirect('/about');
 });
 
+app.get('/blogs/create', (request, response) => {
+    response.render('create', {title: 'New Blog'});
+})
+
 app.use((request, response) => {
-    response.status(404).sendFile('./views/404.html', { root: __dirname });
+    response.status(404).render('404', {title: 'Mr. T Says'});
 });
